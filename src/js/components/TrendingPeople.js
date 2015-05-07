29d14d48b977bda1b08551956cc4769585fe43b8
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-05-07 14:13:14 +0100

    css for trendibuttons

--- src/js/components/TrendingPeople.js
+++ src/js/components/TrendingPeople.js
@@ -3,8 +3,13 @@ var React = require("react");
 var TrendingPeople = React.createClass({
     render: function() {
 
+      var trendiLogo = "./assets/images/logo-small.png";
+
       var trendingPeople = this.props.people.map(function(image, index) {
-        return <img src={image.img} key={Math.random()} className="imageBox"/>;
+        return <div className="imageBox">
+            <img src={image.img} key={Math.random()} />
+            <img src={trendiLogo} /><img src={trendiLogo} /><img src={trendiLogo} /><img src={trendiLogo} /><img src={trendiLogo} />
+          </div>;
       }.bind(this));
         return(
           <div>
