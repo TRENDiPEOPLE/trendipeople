fa3ecb261db75492b27b385257166972b08f03a4
Jasonspd <jasoncluu@googlemail.com>
2015-05-07 16:24:58 +0100

    mongoose crate s3 stuff

--- api/schema.js
+++ api/schema.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var crate = require('mongoose-crate');
 var S3 = require('mongoose-crate-s3');
+var config = require('./config');
 
 var userSchema = new mongoose.Schema({
 	username: String,
@@ -10,11 +11,27 @@ var userSchema = new mongoose.Schema({
 var User = mongoose.model('User', userSchema);
 
 var imageSchema = new mongoose.Schema({
-	imageId: String,
-	userId: String,
-	ratingTotal: Number,
-	ratingCount: Number
+	imageId: String, //Id for images
+	userId: String, //Facebook id
+	ratingTotal: Number, //Total rating score, e.g 4 + 3 + 2 = 9
+	ratingCount: Number //Total number of ratings, e.g. 3 => 9/3 = 3 (average)
 });
+
+// imageSchema.plugin(crate, {
+//   storage: new S3({
+//     key: '<api-key-here>',
+//     secret: '<secret-here>',
+//     bucket: '<bucket-here>',
+//     acl: '<acl-here>', // defaults to public-read
+//     region: '<region-here>', // defaults to us-standard
+//     path: function(attachment) { // where the file is stored in the bucket - defaults to this function
+//       return '/' + path.basename(attachment.path)
+//     }
+//   }),
+//   fields: {
+//     file: {}
+//   }
+// });
 
 var Images = mongoose.model('Images', imageSchema);
 
