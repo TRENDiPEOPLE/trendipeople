f29435e14ccd07c7096f7773b1fa0a8aabf05e93
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-05-18 13:57:32 +0100

    landing page text adding

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var server = require('./api/server');
 
 server.start(function(){
-    console.log('Server running at port ', server.info.uri);
+    console.log('Server running at port: ', server.info.port);
 });
